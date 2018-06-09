113a9bf78bc8d915b3fe43d132643b145fa01e98
Natsukashiii <softtkittty@163.com>
2018-06-09 21:45:56 +0800

    20180609
    9. Palindrome Number

--- LeetCode/src/main/java/Pro09/PalindromeNumber/Solution.java
+++ LeetCode/src/main/java/Pro09/PalindromeNumber/Solution.java
@@ -8,28 +8,54 @@ public class Solution
 {
     public static void main(String[] args)
     {
-        Scanner scanner=new Scanner(System.in);
-        int x=scanner.nextInt();
+        System.out.println("Please enter the number: ");
+        Scanner scanner = new Scanner(System.in);
+//        double x = scanner.nextDouble();
+        int x = scanner.nextInt();
+        scanner.close();
+        Solution solution = new Solution();
+        boolean result = solution.isPalindrome(x);
+        System.out.println("result: " + result);
     }
-    public boolean isPalindrome(int x) {
-        String number=String.valueOf(x);
-        int length=number.length();
 
-        if (x<1||length%2==0){
+    public boolean isPalindrome(int x)
+    {
+        String number = String.valueOf(x);
+        int length = number.length();
+        int l = length;
+
+
+//        如果这个数是小数，或者小于0的话一定不是回文串
+        if (x < 1 & x != 0)
+        {
             return false;
         }
-        else {
-            for (int i = 0; i <(length+1)/2 ; i++)
+//        如果位数为偶数的话，形如1001的情况
+        else if (length % 2 == 0 & length >= 2)
+        {
+            for (int i = 0; i < length/2; i++)
             {
-                for (int j = length; j >(length+1)/2 ; j--)
+                l--;
+                if (number.charAt(i) != number.charAt(l))
                 {
-                    
+                    return false;
                 }
             }
+            return true;
         }
 
-
-
-
+//        形如12121、123454321的情况
+        else
+        {
+            for (int i = 0; i < (length + 1) / 2; i++)
+            {
+                l--;
+                if (number.charAt(i) != number.charAt(l))
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
     }
 }
