157c78973e835be1f66279a9208560e2ab428bee
Natsukashiii <softtkittty@163.com>
2018-06-10 17:37:20 +0800

    10.RegularExpressionMatching
    20180610

--- LeetCode/src/main/java/Pro10/RegularExpressionMatching/Solution.java
+++ LeetCode/src/main/java/Pro10/RegularExpressionMatching/Solution.java
@@ -1,5 +1,33 @@
 package Pro10.RegularExpressionMatching;
 
+/*给定一个字符串 (s) 和一个字符模式 (p)。实现支持 '.' 和 '*' 的正则表达式匹配。
+
+        '.' 匹配任意单个字符。
+        '*' 匹配零个或多个前面的元素。*/
+
+import java.util.HashMap;
+
 public class Solution
 {
+    public boolean isMatch(String s, String p) {
+        String string1=s;
+        String string2=p;
+        int length1=string1.length();
+        int length2=string2.length();
+
+//        如果两个都为空串，返回true
+        if (string1==null&string2==null){
+            return true;
+        }
+
+//        取一个MAP记录*的位置，和字符
+        HashMap<Integer,Character> mapLocation=new HashMap<Integer, Character>();
+
+        for (int i = 0; i <length2 ; i++)
+        {
+            if (string2.charAt(i)==42){
+                mapLocation.put(i,string2.charAt(i-1));
+            }
+        }
+    }
 }
