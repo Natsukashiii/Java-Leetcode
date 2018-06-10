64a033b967b032a4e57bc85d0a2de5d4dbf0be5d
Natsukashiii <softtkittty@163.com>
2018-06-10 21:23:54 +0800

    20180610
    10.RegularExpressionMatching

--- LeetCode/src/main/java/Pro10/RegularExpressionMatching/Solution.java
+++ LeetCode/src/main/java/Pro10/RegularExpressionMatching/Solution.java
@@ -5,6 +5,9 @@ package Pro10.RegularExpressionMatching;
         '.' 匹配任意单个字符。
         '*' 匹配零个或多个前面的元素。*/
 
+//1. 如果p为空，则只有s为空的情况下才匹配成功，否则匹配失败
+//2. ru
+
 import java.util.HashMap;
 
 public class Solution
@@ -16,8 +19,10 @@ public class Solution
         int length2=string2.length();
 
 //        如果两个都为空串，返回true
-        if (string1==null&string2==null){
-            return true;
+        if (string2==null){
+            if (string1==null){
+            return true;}
+            else return false;
         }
 
 //        取一个MAP记录*的位置，和字符
@@ -28,7 +33,7 @@ public class Solution
             if (string2.charAt(i)==42){
                 mapLocation.put(i,string2.charAt(i-1));
             }
-            
+
         }
     }
 }
