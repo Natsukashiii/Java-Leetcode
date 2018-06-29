9eddaaabd5c5644652f8c37af865c750f24ed51a
Natsukashiii <softtkittty@163.com>
2018-06-29 20:45:40 +0800

    20180629

---
+++ LeetCode/src/main/java/Pro17/LetterCombinationsofaPhoneNumber/Solution.java
@@ -0,0 +1,33 @@
+package Pro17.LetterCombinationsofaPhoneNumber;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Solution
+{
+    public static void main(String[] args)
+    {
+        String one = "abc";
+
+    }
+
+    public List<String> letterCombinations(String digits)
+    {
+        String two = "abc";
+        String three = "def";
+        String four = "ghi";
+        String five = "jkl";
+        String six = "mnt";
+        String seven = "pqrs";
+        String eight = "tuv";
+        String nine = "wxyz";
+
+        List<String> res = new ArrayList<String>();
+        int length = digits.length();
+        for (int i = 0; i < length; i++)
+        {
+
+        }
+        return null;
+    }
+}
