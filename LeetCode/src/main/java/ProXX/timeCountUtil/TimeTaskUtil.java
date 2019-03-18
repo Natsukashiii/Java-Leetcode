21a75a98293ab6ed0da0e652a3c73c637dd69d05
Natsukashiii <softtkittty@163.com>
2019-03-18 18:00:42 +0800

    schedule task edit

--- LeetCode/src/main/java/ProXX/timeCountUtil/TimeTaskUtil.java
+++ LeetCode/src/main/java/ProXX/timeCountUtil/TimeTaskUtil.java
@@ -3,23 +3,35 @@ package ProXX.timeCountUtil;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class TimeTaskUtil {
 
 
     private final static String DATE_FORMAT = "yyyyMMddHHmmssSSS";
 
+    public static void timeTask(int time) {
+        ConcurrentHashMap<String, String> timeMap = new ConcurrentHashMap();
+        String currentTime = "";
+        String timeInterval = "";
+        String expectedTime = "";
+
+        timeMap.put("currentTime", currentTime);
+        timeMap.put("timeInterval", timeInterval);
+        timeMap.put("expectedTime", expectedTime);
+    }
+
 
     /**
-     * 计算当前时间加上 给定时间 后的系统时间
-     * @param time
-     */
-    public static void caculateTime(int time) {
+     * 计算预期工作时间
+     **/
+    public static void getExceptedTime(int timeInterval) {
 
     }
 
     /**
      * 获取当前时间
+     *
      * @return
      */
     public static String getCurrentTime() {
@@ -29,6 +41,7 @@ public class TimeTaskUtil {
 
     /**
      * 判断时间大小
+     *
      * @param time1
      * @param time2
      */
@@ -58,8 +71,7 @@ public class TimeTaskUtil {
     /**
      * 计算当前时间到下offset个整点的时间
      *
-     * @param offset
-     *            小时个数，比如计算当前时间到下一个整点的时间，传值为1；到第二个整点，则传值为2；依次类推
+     * @param offset 小时个数，比如计算当前时间到下一个整点的时间，传值为1；到第二个整点，则传值为2；依次类推
      * @return
      */
     public static long nextHourDelay(int offset) {
