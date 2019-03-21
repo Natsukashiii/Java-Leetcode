955e45fcd0480d05a052686a2d468ce618461e1d
Natsukashiii <softtkittty@163.com>
2019-03-21 13:42:06 +0800

    add gitignore

--- JZoffer/src/Pro01/Find/Solution.java
+++ JZoffer/src/Pro01/Find/Solution.java
@@ -1,53 +1,45 @@
 package Pro01.Find;
 
-//从左下角开始遍历，
-//如果这个数大于目标，减少行数
-//如果这个数小于目标，列数增加
 
-public class Solution
-{
-    public boolean Find(int target, int[][] array)
-    {
+/**
+ * 在一个二维数组中（每个一维数组的长度相同），每一行都按照从左到右递增的顺序排序，每一列都按照从上到下递增的顺序排序。
+ * 请完成一个函数，输入这样的一个二维数组和一个整数，判断数组中是否含有该整数。
+ *
+ * Solution: 从左下角开始遍历, 如果这个数大于目标, 减少行数, 如果这个数小于目标, 增加列数
+ */
+public class Solution {
+
+  public boolean Find(int target, int[][] array) {
 //        多少行
-        int row = array.length;
+    int row = array.length;
 //        多少列
-        int col = array[0].length;
+    int col = array[0].length;
 //        i用于遍历行坐标，j用于遍历列坐标
-        int i = row - 1, j = 0;
+    int i = row - 1, j = 0;
 
-        while (i >= 0 && j < col)
-        {
+    while (i >= 0 && j < col) {
 //            从左下角开始寻找
-            int number = array[i][j];
-            if (target == number)
-            {
-                return true;
-            }
-            else if (number > target)
-            {
-                i--;
-            }
-            else
-            {
-                j++;
-            }
-        }
-        return false;
+      int number = array[i][j];
+      if (target == number) {
+        return true;
+      } else if (number > target) {
+        i--;
+      } else {
+        j++;
+      }
     }
+    return false;
+  }
 
 
-    public static void main(String[] args)
-    {
-        Solution solution = new Solution();
-        int[][] array = {{2, 4, 6, 7}, {4, 9, 10, 13}, {6, 10, 15, 20}};
-        if (solution.Find(8, array))
-        {
-            System.out.println("存在");
-        }
-        else
-        {
-            System.out.println("不存在");
-        }
+  public static void main(String[] args) {
+    Solution solution = new Solution();
+    int[][] array = {{2, 4, 6, 7}, {4, 9, 10, 13}, {6, 10, 15, 20}};
+    if (solution.Find(8, array)) {
+      System.out.println("存在");
+    } else {
+      System.out.println("不存在");
     }
+  }
 }
 
--- JZoffer/src/Pro02/ReplaceSpace/Solution.java
+++ JZoffer/src/Pro02/ReplaceSpace/Solution.java
@@ -2,45 +2,38 @@ package Pro02.ReplaceSpace;
 
 //用原数组扩大长度后存储新的数组
 
-public class Solution
-{
-    public String replaceSpace(StringBuffer str)
-    {
-        int spacenum = 0;
-        for (int i = 0; i < str.length(); i++) 
-        
-            if (str.charAt(i) == ' ')
-            {
-                spacenum++;
-            }
-        }
-        int indexold = str.length() - 1;
-        int newlength = str.length() + spacenum * 2; //原来的空格变成'%''2''0' 多了两倍的空格数
-        int indexnew = newlength - 1;
-        str.setLength(newlength);
+public class Solution {
 
-        for (; indexold >= 0 && indexold < newlength; --indexold)
-        {
-            if (str.charAt(indexold) == ' ')
-            {
-                str.setCharAt(indexnew--, '0');
-                str.setCharAt(indexnew--, '2');
-                str.setCharAt(indexnew--, '%');
-            }
-            else
-            {
-                str.setCharAt(indexnew--, str.charAt(indexold));
-            }
-        }
-        return str.toString();
+  public String replaceSpace(StringBuffer str) {
+    int spacenum = 0;
+    for (int i = 0; i < str.length(); i++) {
+
+      if (str.charAt(i) == ' ') {
+        spacenum++;
+      }
+    }
+    int indexold = str.length() - 1;
+    int newlength = str.length() + spacenum * 2; //原来的空格变成'%''2''0' 多了两倍的空格数
+    int indexnew = newlength - 1;
+    str.setLength(newlength);
+
+    for (; indexold >= 0 && indexold < newlength; --indexold) {
+      if (str.charAt(indexold) == ' ') {
+        str.setCharAt(indexnew--, '0');
+        str.setCharAt(indexnew--, '2');
+        str.setCharAt(indexnew--, '%');
+      } else {
+        str.setCharAt(indexnew--, str.charAt(indexold));
+      }
+    }
+    return str.toString();
 //        return str.toString().replaceAll("\\s","%20");
 //        正则
-    }
+  }
 
-    public static void main(String[] args)
-    {
-        Solution solution = new Solution();
-        StringBuffer stringBuffer = new StringBuffer(" hello");
-        System.out.println(solution.replaceSpace(stringBuffer));
-    }
+  public static void main(String[] args) {
+    Solution solution = new Solution();
+    StringBuffer stringBuffer = new StringBuffer(" hello");
+    System.out.println(solution.replaceSpace(stringBuffer));
+  }
 }
